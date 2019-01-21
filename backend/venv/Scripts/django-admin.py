f7111d3d0eb3297dc058397488c31b870fcbfb99
baronkimaru <bizzinali@gmail.com>
2019-01-21 10:31:59 +0300

    initial commit

---
+++ backend/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\baronprojects\pythonprojects\djangoprojects\djangoreact\backend\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
